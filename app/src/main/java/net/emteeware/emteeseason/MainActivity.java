6737a37c3b41f26f57463cf141086e3dc4f6b32a
Merlin Thomas <dev@emteeware.net>
2017-07-01 20:11:47 +0200

    Implement prototypical list view

--- app/src/main/java/net/emteeware/emteeseason/MainActivity.java
+++ app/src/main/java/net/emteeware/emteeseason/MainActivity.java
@@ -2,12 +2,28 @@ package net.emteeware.emteeseason;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import static net.emteeware.emteeseason.R.id.SeriesList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listview = (ListView) findViewById(R.id.SeriesList);
+
+        String[] items = new String[] {"Blue Bloods", "Marvel's Daredevil", "Suits"};
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+        listview.setAdapter(adapter);
     }
 }
