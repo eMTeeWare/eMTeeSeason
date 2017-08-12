8a8dfc571b4922a74ba71932d9104e80cd11ad3d
Merlin Thomas <dev@emteeware.net>
2017-08-12 12:00:05 +0200

    Ask for item to be added

--- app/src/main/java/net/emteeware/emteeseason/MainActivity.java
+++ app/src/main/java/net/emteeware/emteeseason/MainActivity.java
@@ -1,10 +1,15 @@
 package net.emteeware.emteeseason;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -21,10 +26,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        items.add( "Suits");
         ListView listview = (ListView) findViewById(R.id.SeriesList);
 
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
@@ -34,8 +40,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addItems(View view) {
-        items.add("Blue Bloods");
-        items.add("Sherlock");
-        adapter.notifyDataSetChanged();
+        final String[] addedSeriesName = {""};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Name der neuen Serie:");
+
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                addedSeriesName[0] = input.getText().toString();
+                items.add(addedSeriesName[0]);
+                adapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
     }
 }
